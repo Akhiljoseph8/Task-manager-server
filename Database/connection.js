6b405d021774ad41168a3fe69da6960b10bc7fe5
Akhil Joseph <120910561+Akhiljoseph8@users.noreply.github.com>
2024-08-09 22:39:33 +0530

    Update connection.js

--- Database/connection.js
+++ Database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const connectionString = process.env.DATABASE
+const connectionString = "mongodb+srv://akhiljoseph:akhiljoseph@cluster0.hoizh.mongodb.net/TASKMANAGER?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.connect(connectionString).then(() => {
     console.log("MongoDB connection Success")
 }).catch((err) => {
